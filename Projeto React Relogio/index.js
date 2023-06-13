ba31e1263a0d7adb4146847937c166e7e839ff77
Victor Hugo Santos <133922486+VictorSantos29@users.noreply.github.com>
2023-06-13 19:15:41 -0300

    Add files via upload

---
+++ Projeto React Relogio/index.js
@@ -0,0 +1,11 @@
+import react from 'react';
+import reactDOM from 'react-dom';
+import "./index.css";
+import app from "./relogio";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+        </React.StrictMode>
+        document.getElementById("root")
+);
